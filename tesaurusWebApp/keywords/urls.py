72bfb5e99555c6628802ed801fcbc150180fcd2e
Javier Sopesens <jsopesens@iphes.cat>
2022-07-05 15:24:58 +0200

    preparing urls

--- tesaurusWebApp/keywords/urls.py
+++ tesaurusWebApp/keywords/urls.py
@@ -5,5 +5,6 @@ from . import views
 # every keyword page
 # instead of redirecting one by one
 urlpatterns = [
+    path("", views.index),
     path("<keyword>", views.keywords)
 ]
--- tesaurusWebApp/keywords/views.py
+++ tesaurusWebApp/keywords/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
-# Create your views here.
+
+
+
+
+def index(request):
+    return HttpResponse("aqui se leera el doc owl con Owlready")
+    
+
 
 def keywords(request, keyword):
     return HttpResponse(keyword)
