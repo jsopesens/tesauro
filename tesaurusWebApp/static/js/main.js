b8779f111bd1d3a03619f2ad529f8035d828fefc
Javier Sopesens <jsopesens@iphes.cat>
2022-07-21 11:08:44 +0200

    ajax call to retrieve matching content

--- tesaurusWebApp/static/js/main.js
+++ tesaurusWebApp/static/js/main.js
@@ -1,24 +1,22 @@
 var searchInput = document.getElementById('searchQueryInput')
 var endpoint = '/keywords/getMatchKeywords/'
 
-searchInput.addEventListener('input', function(){
-    getMatchKeywords(searchInput.value)})
+searchInput.addEventListener('input', function () {
+    showMatchKeywords(searchInput.value);
+})
 
-function getMatchKeywords(searchInput){
+function showMatchKeywords(searchText) {
+    let keywordsList = document.getElementById('keywords_list')
+    let keywordsContainer = document.getElementById('navBarContainer')
     // GET MODE
-    fetch(endpoint+searchInput)
-    .then(response => response.json())
-    .then(data=>{
-        var allLinks
-        data.keywords.forEach(element => {
-            allLinks +='<li><a href="'+element+'">'+element+'</a></li>'            
-        });
-        document.getElementById("keywords").innerHTML = allLinks
-    })
-
-    keywordsDOM = document.getElementById('keywords')
-    keywordsDOM.style.display = !searchInput ? 'none': 'block'
-
+    // search input with only whitespaces generates errors on the HTTP request
+    if (searchText.trim() != '') {
+        fetch(endpoint + searchText)
+            .then(keywordsList.innerHTML = '')
+            .then(response => response.json())
+            .then(data => convertToListElements(data.keywords))
+    } else
+        keywordsContainer.style.display = 'none'
     // POST MODE
     // input = this.value,
     // fetch( endpoint+this.value,{
@@ -31,4 +29,29 @@ function getMatchKeywords(searchInput){
     // .then(data=>{
     //     console.log(data)
     // })
+}
+
+function convertToListElements(data) {
+    data.forEach(element => {
+        const li = document.createElement('li')
+        const a = document.createElement('a')
+        li.classList.add('keyword_suggestion')
+        a.href = element
+        a.innerHTML = element
+        li.appendChild(a)
+        document.getElementById('keywords_list').appendChild(li)
+    });
+    // I SUSPECT THAT THERE IS NOT THE BEST PLACE FOR THIS FUNCTION.
+    // I HAVE TO TRY TO CHANGE HIS PLACE
+    showSuggestions()
+}
+
+function showSuggestions() {
+    const keywordDOM = document.getElementById('navBarContainer')
+    const keywordsList = document.getElementById('keywords_list')
+
+    keywordDOM.style.display =
+        (keywordsList.childElementCount === 0 || searchInput.value == '') ?
+            'none' :
+            'block'
 }
