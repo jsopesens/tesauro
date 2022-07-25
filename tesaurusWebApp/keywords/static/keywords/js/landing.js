be98b65e3304a412866e58ef0dc374cb6d96aed0
Javier Sopesens <jsopesens@iphes.cat>
2022-07-25 12:57:37 +0200

    clean landing js

--- tesaurusWebApp/keywords/static/keywords/js/landing.js
+++ tesaurusWebApp/keywords/static/keywords/js/landing.js
@@ -3,51 +3,54 @@
 // At clicking on svg element, we aren't redirected
 var fullKeywordList = document.getElementById('fullListKeywords')
 fullKeywordList.addEventListener('click', e => {
-    if(e.target && e.target.matches('li.keyword')){
+    if (e.target && e.target.matches('li.keyword')) {
         e.target.children[0].click()
     }
 })
 
 // click on svg to ajax call childs
-fullKeywordList.addEventListener('click', e =>{
-    if(e.target && e.target.matches('svg.showMore')){
-        let father    = e.path[1]
-        let svg       = e.path[0]
-        let innerList = father.querySelector('ul')
+fullKeywordList.addEventListener('click', e => {
+    if (e.target && e.target.matches('svg.showMore')) {
+        let fatherKeyword = e.path[1]
+        let svg = e.path[0]
+        let innerList = fatherKeyword.querySelector('ul')
         let endpoint = '/keywords/getSonsOf/'
-        let keyword  = e.path[1].id
+        let keywordName = e.path[1].id
+        
+        rotateSVG(svg)
 
-        // is list of sons visible?
-        // if it is, hide it
-        if(innerList && innerList.style.display == 'block'){
-            svg.setAttribute('transform', 'rotate(0)')
-            innerList.style.display = 'none'
-        }
-        // isn't visible, so show it
-        else{
-            // First Deployment? so do ajax call
-            if(!innerList){
-                fetch(endpoint + keyword)
+        if (!innerList) {
+            // generate content and show it
+            fetch(endpoint + keywordName)
                 .then(response => response.json())
-                .then(data => showSons(e, data))
-            }
-
-            // not first deployment, only need to show it
-            svg.setAttribute('transform', 'rotate(90)')
-            innerList.style.display = 'block' 
+                .then(data => showSons(fatherKeyword, data))
+        }
+        if(innerList){
+            // only need to hide or show it
+            displayListContent(innerList)
         }
     }
 })
 
-function showSons(e, data){
-    parentElement = e.path[1]
+function displayListContent(list){
+    const currentDisplay = list.style.display 
+    list.style.display = currentDisplay == 'block' ? 'none': 'block'
+}
+
+function rotateSVG(svg) {
+    const rotation = svg.getAttribute('transform')
+    svg.setAttribute('transform', rotation == 'rotate(0)' ? 'rotate(90)' : 'rotate(0)')
+}
+
+function showSons(fatherKeyword, data) {
     const lu = document.createElement('ul')
-    parentElement.appendChild(lu)
+    lu.style.display = 'block'
+    fatherKeyword.appendChild(lu)
     data['topConcepts'].forEach(element => {
         const li = document.createElement('li')
-        const a  = document.createElement('a')
+        const a = document.createElement('a')
         li.classList = 'keyword'
-        a.href      = element
+        a.href = element
         a.innerHTML = element
         li.appendChild(a)
         lu.appendChild(li)
